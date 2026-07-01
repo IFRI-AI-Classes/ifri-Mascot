ecfe76468d59f8dc42184e9bb75993fcac2c019c
thelord00229 <fergalkouliho1@gmail.com>
2026-07-01 17:31:45 +0100

    Branche les commandes web aux mouvements

--- firmware/src/WebServer.cpp
+++ firmware/src/WebServer.cpp
@@ -6,24 +6,54 @@
 #include "Pinout.h"
 #include "BatteryDriver.h"
 #include "UltrasonicDriver.h"
-
-// Declarations externes des autres modules
-extern void servoDriverInit();
-extern void servoDriverUpdate();
-extern void motionEngineInit();
-extern void motionEngineUpdate();
-extern void ultrasonicDriverInit();
-extern void ultrasonicDriverUpdate();
-extern void audioDriverInit();
-extern void audioDriverUpdate();
+#include "MotionEngine.h"
 
 static AsyncWebServer server(WEB_SERVER_PORT);
 
-static String currentState = "Marche";
+static String currentState = "Arret";
 static String currentSpeed = "Normal";
 static bool isMuted = false;
 static int audioVolume = 50;
 static String currentMoveCmd = "stop";
+
+static const char* motionModeToString(MotionMode mode) {
+    switch (mode) {
+        case MOTION_WALK_FORWARD:
+            return "Avant";
+        case MOTION_WALK_BACKWARD:
+            return "Arriere";
+        case MOTION_TURN_LEFT:
+            return "Gauche";
+        case MOTION_TURN_RIGHT:
+            return "Droite";
+        case MOTION_ROLL:
+            return "Roulis";
+        case MOTION_IDLE:
+        default:
+            return "Arret";
+    }
+}
+
+static bool applyMoveCommand(const String& cmd) {
+    if (cmd == "forward") {
+        motionStart(MOTION_WALK_FORWARD, WALK_SPEED_NORMAL);
+    } else if (cmd == "backward") {
+        motionStart(MOTION_WALK_BACKWARD, WALK_SPEED_NORMAL);
+    } else if (cmd == "left") {
+        motionStart(MOTION_TURN_LEFT, WALK_SPEED_NORMAL);
+    } else if (cmd == "right") {
+        motionStart(MOTION_TURN_RIGHT, WALK_SPEED_NORMAL);
+    } else if (cmd == "stop") {
+        motionStop();
+    } else {
+        return false;
+    }
+
+    currentMoveCmd = cmd;
+    currentState = motionModeToString(motionGetCurrentMode());
+    currentSpeed = motionIsMoving() ? "Normal" : "Arret";
+    return true;
+}
 
 void webServerInit() {
     if (!SPIFFS.begin(true)) {
@@ -48,10 +78,14 @@ void webServerInit() {
         }
 
         String cmd = request->getParam("cmd")->value();
-        currentMoveCmd = cmd;
 
         Serial.print("[API] Move: ");
         Serial.println(cmd);
+
+        if (!applyMoveCommand(cmd)) {
+            request->send(400, "application/json", "{\"status\":\"error\",\"msg\":\"commande inconnue\"}");
+            return;
+        }
 
         String json = "{\"status\":\"ok\",\"cmd\":\"" + cmd + "\"}";
         request->send(200, "application/json", json);
@@ -68,10 +102,13 @@ void webServerInit() {
 
         if (type == "music") {
             msg = "Musique activee";
+            motionDanceSequence();
         } else if (type == "wave") {
             msg = "Salut !";
+            motionGreetSequence();
         } else if (type == "sleep") {
             msg = "Mode veille";
+            motionStop();
             currentState = "Veille";
         } else if (type == "light") {
             msg = "Lumiere togglee";
@@ -102,6 +139,9 @@ void webServerInit() {
     });
 
     server.on("/api/status", HTTP_GET, [](AsyncWebServerRequest *request) {
+        currentState = motionModeToString(motionGetCurrentMode());
+        currentSpeed = motionIsMoving() ? "Normal" : "Arret";
+
         String json = "{";
         json += "\"distance\":" + String(ultrasonicDriverGetDistanceCm(), 1) + ",";
         json += "\"distanceValid\":" + String(ultrasonicDriverHasValidMeasure() ? "true" : "false") + ",";
@@ -112,6 +152,7 @@ void webServerInit() {
         json += "\"batteryCritical\":" + String(batteryIsCritical() ? "true" : "false") + ",";
         json += "\"state\":\"" + currentState + "\",";
         json += "\"speed\":\"" + currentSpeed + "\",";
+        json += "\"move\":\"" + currentMoveCmd + "\",";
         json += "\"muted\":" + String(isMuted ? "true" : "false") + ",";
         json += "\"volume\":" + String(audioVolume);
         json += "}";
